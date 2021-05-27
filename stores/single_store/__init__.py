412f25ff0b08d739ad05652803c55c148d1200db
Susan Sujakhu <52886593+Susansujakhu@users.noreply.github.com>
2021-05-27 21:20:32 +0545

    init - mail and config initialization

--- stores/single_store/__init__.py
+++ stores/single_store/__init__.py
@@ -3,9 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_mail import Mail
+from flask_mail import Mail, Message
 
-from flask_login import UserMixin
 app = Flask(__name__,
 	template_folder='templates',
 	static_folder='static')
