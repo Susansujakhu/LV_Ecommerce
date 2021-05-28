5dbd5440edfa8f8387be1879455e0c192ae05c90
tamakhusunder <tamakhusunder@gmail.com>
2021-05-28 10:52:13 +0545

    stores - init.py resolved

--- stores/single_store/__init__.py
+++ stores/single_store/__init__.py
@@ -8,6 +8,20 @@ from flask_mail import Mail, Message
 app = Flask(__name__,
 	template_folder='templates',
 	static_folder='static')
+app.config.from_object("config.DevelopmentConfig")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sushansujakhu14@localhost/ecommerce'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# # POSTGRES = {
+# #     'user': 'postgres',
+# #     'pw': 'sushansujakhu14',
+# #     'db': 'flask_test',
+# #     'host': 'localhost',
+# #     'port': '5432',
+# # }
+# # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+# app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+# # ...app config...
+# print(app.config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
